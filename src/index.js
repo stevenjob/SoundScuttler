0b0a88dfbfb54926de5cfa3f06852144659cbed5
Steven Job <steven_job@live.com>
2016-07-21 01:00:03 +0100

    Add dependencies for project
    Webpack 2
    React hot loader 3
    eslint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Sound Scuttler');
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from 'components/App'
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <AppContainer>
+    <App/>
+  </AppContainer>,
+  rootElement
+);
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('components/App', () => {
+    const NextApp = require('components/App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp/>
+      </AppContainer>,
+      rootElement
+    );
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
+  devtool: 'eval',
   entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:80',
+    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
-    path: __dirname + '/dist',
+    path: path.join(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
-  devServer: {
-    contentBase: './dist'
-  }
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'eslint'
+    }],
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['', '.js']
+  },
+  eslint: {
+    configFile: './.eslintrc.js'
+  },
 };
